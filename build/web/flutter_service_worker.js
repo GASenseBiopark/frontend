36b191d2bbcb4bffa757229aae97c7ae3d27d6ae
billnobill <106353903+BillNobill@users.noreply.github.com>
2025-06-23 15:12:40 -0300

    Atualização da página WEB.

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1e0ab87e0283a1bfe18ab021bb52f81f
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-messaging-sw.js": "2d9771f024b895c0c09a6ed020bb5d03",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "70c32c6b12ef83fd44e730f099dfda7f",
+"flutter_bootstrap.js": "03c6ed4c0f98798fe0b0c46e20444bd5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
